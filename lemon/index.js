b80514724a7543ab51cf71245633bcbc12e43386
nikita.lipatov <nikita.lipatov@gmail.com>
2023-08-15 13:40:30 +0100

    replace px->rem in CSS

--- lemon/index.js
+++ lemon/index.js
@@ -1,3 +1,9 @@
+function byDefault() {
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("about").style.display = "none";
+    document.getElementById("home").style.display = "flex";
+}
+
 // The difference between style.visibility and style.display:
 // * visibility:hidden renders the tag, but isn't seen on the page.
 // * style.display doesn't render the tag.
