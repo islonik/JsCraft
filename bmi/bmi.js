f0dc5b6858e84111150cf708b4849ebd66dd3cc1
nikita.lipatov <nikita.lipatov@gmail.com>
2023-08-23 10:53:22 +0100

    add statusByBmi in bmi

--- bmi/bmi.js
+++ bmi/bmi.js
@@ -1,4 +1,4 @@
-function bmiCalculate(weight, height, isImperial) {
+function bmiCalculate(weight, height, isImperial = false) {
     result = weight / (height * height);
     if (isImperial) {
         return Math.round(result * 703 * 100) / 100;
@@ -7,4 +7,32 @@ function bmiCalculate(weight, height, isImperial) {
     }
 }
 
-module.exports = bmiCalculate;
+function statusByBmi(bmi, isWhite = true) {
+    return isWhite ? whiteStatusByBmi(bmi) : nonWhiteStatusByBmi(bmi);
+}
+
+function whiteStatusByBmi(bmi) {
+    if (bmi < 18.5) {
+        return "underweight"
+    } else if (18.5 <= bmi && bmi < 24.9) {
+        return "healthy";
+    } else if (24.9 <= bmi && bmi < 30) {
+        return "overweight";
+    } else if (30 <= bmi) {
+        return "obese";
+    }
+}
+
+function nonWhiteStatusByBmi(bmi) {
+    if (bmi < 18.5) {
+        return "underweight"
+    } else if (18.5 <= bmi && bmi < 23) {
+        return "healthy";
+    } else if (23 <= bmi && bmi < 27.5) {
+        return "overweight";
+    } else if (27.5 <= bmi) {
+        return "obese";
+    }
+}
+
+module.exports = {bmiCalculate, statusByBmi};
--- bmi/bmi.test.js
+++ bmi/bmi.test.js
@@ -1,4 +1,4 @@
-const bmiCalculate = require('./bmi.js');
+const { bmiCalculate, statusByBmi } = require('./bmi');
 
 test("test BMI", () => {
     expect(bmiCalculate(75, 1.75, false)).toBe(24.49);
@@ -6,4 +6,52 @@ test("test BMI", () => {
 
 test("test BMI in America", () => {
     expect(bmiCalculate(150, 65, true)).toBe(24.96);
+});
+
+test("white: 18.4 is underweigh", () => {
+    expect(statusByBmi(18.4)).toBe('underweight');
+});
+
+test("white: 18.5 is healthy", () => {
+    expect(statusByBmi(18.5)).toBe('healthy');
+});
+
+test("white: 24.8 is healthy", () => {
+    expect(statusByBmi(24.8)).toBe('healthy');
+});
+
+test("white: 24.9 is healthy", () => {
+    expect(statusByBmi(24.9)).toBe('overweight');
+});
+
+test("white: 29.9 is healthy", () => {
+    expect(statusByBmi(29.9)).toBe('overweight');
+});
+
+test("white: 30 is obese", () => {
+    expect(statusByBmi(30)).toBe('obese');
+});
+
+test("nonWhite: 18.4 is underweigh", () => {
+    expect(statusByBmi(18.4, false)).toBe('underweight');
+});
+
+test("nonWhite: 18.5 is healthy", () => {
+    expect(statusByBmi(18.5, false)).toBe('healthy');
+});
+
+test("nonWhite: 22.9 is healthy", () => {
+    expect(statusByBmi(22.9, false)).toBe('healthy');
+});
+
+test("nonWhite: 23 is healthy", () => {
+    expect(statusByBmi(23, false)).toBe('overweight');
+});
+
+test("nonWhite: 27.4 is healthy", () => {
+    expect(statusByBmi(27.4, false)).toBe('overweight');
+});
+
+test("nonWhite: 27.5 is obese", () => {
+    expect(statusByBmi(27.5, false)).toBe('obese');
 });
