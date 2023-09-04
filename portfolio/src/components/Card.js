308028790f5133a11d2c5bef0adf330f4c7dcf1a
nikita.lipatov <nikita.lipatov@gmail.com>
2023-09-04 19:04:33 +0100

    finish 3rd step

--- portfolio/src/components/Card.js
+++ portfolio/src/components/Card.js
@@ -7,7 +7,17 @@ const Card = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  return (
+    <VStack alignItems="start" className="card">
+      <Image className="image" src={imageSrc} alt={title}/>
+      <Heading as='h4' size='md' className="title">{title}</Heading>
+      <Text className="desc">{description}</Text>
+      <HStack className="see">
+        <Text >See more</Text>
+        <FontAwesomeIcon icon={faArrowRight} size="1x" />
+      </HStack>
+    </VStack>
+  );
 };
 
 export default Card;
--- portfolio/src/components/ProjectsSection.js
+++ portfolio/src/components/ProjectsSection.js
@@ -5,24 +5,28 @@ import Card from "./Card";
 
 const projects = [
   {
+    key: 1,
     title: "React Space",
     description:
       "Handy tool belt to create amazing AR components in a React app, with redux integration via middleware️",
     getImageSrc: () => require("../images/photo1.jpg"),
   },
   {
+    key: 2,
     title: "React Infinite Scroll",
     description:
       "A scrollable bottom sheet with virtualisation support, native animations at 60 FPS and fully implemented in JS land 🔥️",
     getImageSrc: () => require("../images/photo2.jpg"),
   },
   {
+    key: 3,
     title: "Photo Gallery",
     description:
       "A One-stop shop for photographers to share and monetize their photos, allowing them to have a second source of income",
     getImageSrc: () => require("../images/photo3.jpg"),
   },
   {
+    key: 4,
     title: "Event planner",
     description:
       "A mobile application for leisure seekers to discover unique events and activities in their city with a few taps",
@@ -49,7 +53,7 @@ const ProjectsSection = () => {
       >
         {projects.map((project) => (
           <Card
-            key={project.title}
+            key={project.key}
             title={project.title}
             description={project.description}
             imageSrc={project.getImageSrc()}
