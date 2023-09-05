a9bb2cbb9d10c17baf0f73a6d3256fce5cd72c50
nikita.lipatov <nikita.lipatov@gmail.com>
2023-09-05 11:40:49 +0100

    remove comment

--- portfolio/src/components/Card.js
+++ portfolio/src/components/Card.js
@@ -4,9 +4,6 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
 const Card = ({ title, description, preview, imageSrc }) => {
-  // Implement the UI for the Card component according to the instructions.
-  // You should be able to implement the component with the elements imported above.
-  // Feel free to import other UI components from Chakra UI if you wish to.
   return (
     <VStack alignItems="start" className="card">
       <Image className="image" src={preview} alt={title}/>
